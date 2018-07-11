db128de45bbfff4230c1197dda5e220ddf61a4b3
mmorth <mmorth98@gmail.com>
2018-07-10 20:05:02 -0500

    Added Player inputs, services, and team players.

--- client/src/app/services/player.service.ts
+++ client/src/app/services/player.service.ts
@@ -33,21 +33,21 @@ export class PlayerService {
   }
 
   /**
-   * Makes a REST API call to create a team with the given information from the back-end database
-   * @param teamName The name of the team
-   * @param offRating The offensive rating of the team
-   * @param defRating The defensive rating of the team
+   * Makes a REST API call to create a player with the given information from the back-end database
+   * @param playerName The name of the player
+   * @param offRating The offensive rating of the player
+   * @param defRating The defensive rating of the player
    */
-  createPlayer(teamNumber: number, teamName: string, offRating: number, defRating: number) {
+  createPlayer(teamNumber: number, playerName: string, offRating: number, defRating: number) {
   	return this.http.post('http://localhost:8080/api/teams/' + teamNumber + '/players',
-  		JSON.stringify({ "name": teamName, "offensiveRating": offRating, "defensiveRating": defRating }), 
+  		JSON.stringify({ "name": playerName, "offensiveRating": offRating, "defensiveRating": defRating }), 
   		HEADERS
   	);
   }
 
   /**
    * Makes a REST API call to delete the team with the specified id from the back-end database
-   * @param teamID The id of the team to delete
+   * @param playerID The id of the player to delete
    */
   deletePlayer(teamID: number, playerID: number) {
   	return this.http.delete('http://localhost:8080/api/teams/' + teamID + '/' + playerID);
@@ -55,14 +55,14 @@ export class PlayerService {
 
   /**
    * Makes a REST API call to update the team with the given id from the back-end database
-   * @param teamID The id of the team
-   * @param teamName The name of the team
-   * @param offRating The offensive rating of the team
-   * @param defRating The defensive rating of the team
+   * @param playerID The id of the player
+   * @param playerName The name of the player
+   * @param offRating The offensive rating of the player
+   * @param defRating The defensive rating of the player
    */
-  updatePlayer(teamID: number, teamName: string, offRating: number, defRating: number) {
+  updatePlayer(teamID: number, playerName: string, offRating: number, defRating: number) {
   	return this.http.put('http://localhost:8080/api/teams/' + teamID,
-  		JSON.stringify({ "name": teamName, "offensiveRating": offRating, "defensiveRating": defRating }), 
+  		JSON.stringify({ "name": playerName, "offensiveRating": offRating, "defensiveRating": defRating }), 
   		HEADERS
   	);
   }
